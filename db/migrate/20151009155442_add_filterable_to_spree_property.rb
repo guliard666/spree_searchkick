6560d4d70b32fa2ce93d3cb4fb896b98f81dd973
Oleg Zavorotnii <zavorotnii@gmail.com>
2024-11-05 09:16:42 +0200

    Fixed migrations

--- db/migrate/20151009155442_add_filterable_to_spree_property.rb
+++ db/migrate/20151009155442_add_filterable_to_spree_property.rb
@@ -1,5 +1,5 @@
 class AddFilterableToSpreeProperty < ActiveRecord::Migration[5.1]
   def change
-    #add_column :spree_properties, :filterable, :boolean, if_not_exists: true
+    add_column :spree_properties, :filterable, :boolean, if_not_exists: true
   end
 end
